b71252a451c0b6282468fe208d3a75ece0e314b3
JankMajesty <lmcgill36@gmail.com>
2021-10-28 23:27:24 -0500

    flexbox in add listing

--- src/components/AddListing.js
+++ src/components/AddListing.js
@@ -115,7 +115,9 @@ const AddListing = (props) => {
      name: '',
      address: '',
      hours: '',
-     description: ''
+     description: '',
+     lat: '',
+     lng: ''
  } )
  
    // input field change handler based on their id
@@ -139,19 +141,23 @@ const AddListing = (props) => {
        name: '',
        address: '',
        hours: '',
-       description: ''
+       description: '',
+       lat: '',
+    lng: ''
    } )
  }
  
  
     return (
-      <div className="flex-row login-button">
-        <div>
+      <div className='flex-container' >
+        <div className="flex-child login-button">
          <form className="add-listing" onSubmit={handleSubmit} action="/listings">
              <TextField value={listing.name} required="true" id="name" onChange={handleTextChange} placeholder="Name" />
              <TextField value={listing.address} required="true" id="address" onChange={handleTextChange} placeholder="Address" />
              <TextField value={listing.hours} required="true" id="hours" onChange={handleTextChange} placeholder="Hours" />
              <TextField value={listing.description} required="true" id="description" onChange={handleTextChange} placeholder="Description" />
+             <TextField value={listing.lat} required="true" id="lat" onChange={handleTextChange} placeholder="Latitude" />
+             <TextField value={listing.lng} required="true" id="lng" onChange={handleTextChange} placeholder="Longitude" />
                <div className="margin-top-sm button-wide">
              <Box mt={4}>   
              <Button style={{width: '400px'}}
@@ -165,7 +171,7 @@ const AddListing = (props) => {
              </div>
            </form>
            </div>
-           <div style={{ height: '50vh', width: '50%' }}>
+           <div className='flex-child' style={{ height: '50vh', width: '50%' }} >
            <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyDjULikPHmrPzrmZbtyphQaevW2iqFUWPU'}}
           defaultCenter={{
